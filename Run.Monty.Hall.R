726b4d1704d81a6d02bce8ac0e0a582a01f6c3f3
Hiba-N <hibanasir123@hotmail.com>
2026-02-24 16:01:34 -0500

    comments added

--- Run.Monty.Hall.R
+++ Run.Monty.Hall.R
@@ -6,19 +6,18 @@ run.monty.hall <- function(args){
 
     allowed.arguments <- c("Keep", "Switch")
 
-    if (length(args) != 1 || !(args[1] %in% allowed.arguments)) {
+    if (length(args) != 1 || !(args[1] %in% allowed.arguments)) { #checking input by user
         cat("usage example: Rscript Run.Monty.Hall.R Switch/Keep\n")
         quit()
         }
 
     players.decision <- args[1]
-    print(players.decision)
 
-    if(players.decision == "Keep"){
+    if(players.decision == "Keep"){ #running monty hall without switching door
         cat("When the door is not switched, the fraction of wins is", play.many.monty.halls(100, FALSE))
     }
 
-    if(players.decision == 'Switch'){
+    if(players.decision == 'Switch'){ #running monty hall with door switched
         cat("When the door is switched, the fraction of wins is", play.many.monty.halls(100, TRUE))
     }
 
