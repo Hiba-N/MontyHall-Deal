9ba861ae8283499bededbe8dfc84e2c5b21c5455
Hiba-N <hibanasir123@hotmail.com>
2026-02-24 11:33:51 -0500

    tested existing functions, added new function play.many.monty.halls

--- Monty.Hall.Utilities.R
+++ Monty.Hall.Utilities.R
@@ -11,7 +11,7 @@ opened.door <- function(car.door, contestant.door){
     Montys.door <- sample(3, 1)
 
     #ensuring Monty's door is other than contestant door or door with car behind it
-    while(Montys.door = car.door || Montys.door = contestant.door){
+    while(Montys.door == car.door || Montys.door == contestant.door){
         Montys.door <- sample(3, 1)
     }   
     return(Montys.door)
@@ -27,11 +27,31 @@ play.monty.hall <- function(player.switches){
         contestant.door <- all.doors[-c(montys.door, contestant.door)]
     }
 
-    if(contestant.door = car.door){ #checking if players door has a car
+    if(contestant.door == car.door){ #checking if players door has a car
         win.status <- TRUE
     }
-    else(){
+    else{
         win.status <- FALSE
     }
     return (win.status) 
+}
+
+
+# 1d) Create a function called play.many.monty.halls. This function takes two arguments,  n
+#  , an integer, and a boolean. This function will call the play.monty.hall function  n
+#   times, using the value of the boolean given as an argument. 
+#   The function will return the fraction of times the contestant chooses the winning door.
+
+play.many.monty.halls <- function(total.plays, player.switches){
+    play.count <- 0
+    total.wins <- 0
+    while (play.count < total.plays){
+        
+        win.status <- play.monty.hall(player.switches)
+        total.wins <- win.status + total.wins
+        play.count <- play.count + 1
+    }
+
+    winning.fraction <- total.wins/total.plays
+    return(winning.fraction)
 }
