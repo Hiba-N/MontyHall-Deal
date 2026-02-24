d6d9a19d49cf8eb3fe850e2324ac86979898d76c
Hiba-N <hibanasir123@hotmail.com>
2026-02-24 16:00:29 -0500

    modularity added, testing complete

--- Run.Monty.Hall.R
+++ Run.Monty.Hall.R
@@ -1,22 +1,29 @@
 source("Monty.Hall.Utilities.R")
 args <- commandArgs(trailingOnly = TRUE)
 
-allowed.arguments <- c("Keep", "Switch")
 
-if (length(args) != 1 || !(args[1] %in% allowed.arguments)) {
-    cat("usage example: Rscript Run.Monty.Hall.R Switch/Keep\n")
-    quit()
+run.monty.hall <- function(args){
+
+    allowed.arguments <- c("Keep", "Switch")
+
+    if (length(args) != 1 || !(args[1] %in% allowed.arguments)) {
+        cat("usage example: Rscript Run.Monty.Hall.R Switch/Keep\n")
+        quit()
+        }
+
+    players.decision <- args[1]
+    print(players.decision)
+
+    if(players.decision == "Keep"){
+        cat("When the door is not switched, the fraction of wins is", play.many.monty.halls(100, FALSE))
     }
 
-players.decision <- args[1]
+    if(players.decision == 'Switch'){
+        cat("When the door is switched, the fraction of wins is", play.many.monty.halls(100, TRUE))
+    }
 
-
-if(players.decision == "Keep"){
-    cat("When the door is not switched, the fraction of wins is" + play.many.monty.halls(100, FALSE))
 }
 
-elif(players.decision == 'Switch'){
-    cat("When the door is switched, the fraction of wins is" + play.many.monty.halls(100, TRUE))
-}
+run.monty.hall(args)
 
 
